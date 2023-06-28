59ee8284337339fa489da90a1418b9f36ec38c72
alex-dchlecht <alexschlecht@yandex.ru>
2023-06-28 12:39:05 +0300

    test commit

--- backend/controllers/pageNotFound.js
+++ backend/controllers/pageNotFound.js
@@ -1,5 +1,5 @@
-const PageNotFound = require('../errors/PageNotFound');
-
-module.exports.PageNotFound = (req, res, next) => {
-  next(new PageNotFound('Page Not Found'));
+module.exports.pageNotFound = (req, res) => {
+  res.status(404).send({
+    message: 'Страница не найдена',
+  });
 };
