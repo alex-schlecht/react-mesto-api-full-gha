1be8147402358be986cd6e5b540d6aefeacb095c
alex-dchlecht <alexschlecht@yandex.ru>
2023-06-28 09:29:37 +0300

    Test commit

--- backend/routes/pageNotFound.js
+++ backend/routes/pageNotFound.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { pageNotFound } = require('../controllers/PageNotFound');
+const { pageNotFound } = require('../controllers/pageNotFound');
 
 router.all('/*', pageNotFound);
 
