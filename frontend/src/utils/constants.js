3fd0f9cff4c152e504df9eeb18197efe67efb514
alex-dchlecht <alexschlecht@yandex.ru>
2023-07-01 23:27:08 +0300

    test

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
 const { NODE_ENV } = process.env;
-module.exports.API_URL = NODE_ENV === 'production' ? '//api.mestoapi.nomoreparties.sbs' : 'http://localhost:3000';
+module.exports.API_URL = NODE_ENV === 'production' ? 'api.mestoapi.nomoreparties.sbs' : 'http://localhost:3000';
