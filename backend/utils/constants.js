344e66a13d8fa7b1356a04d60c9a54a8306e4344
alex-dchlecht <alexschlecht@yandex.ru>
2023-07-02 00:51:57 +0300

    test

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -2,16 +2,16 @@ module.exports.URI_REGEX = /^(http|https):\/\/(\d|\w)[0-9a-zA-Z./-]+\.[0-9a-zA-Z
 
 module.exports.DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,HEAD,DELETE';
 module.exports.ALLOWED_DOMAIN_NAME = [
-  'http://mestoapi.nomoreparties.sbs/',
-  'https://mestoapi.nomoreparties.sbs/',
-  'http://api.mestoapi.nomoreparties.sbs/',
-  'https://api.mestoapi.nomoreparties.sbs/',
-  'http://www.mestoapi.nomoreparties.sbs/',
-  'https://www.mestoapi.nomoreparties.sbs/',
-  'http://www.api.mestoapi.nomoreparties.sbs/',
-  'https://www.api.mestoapi.nomoreparties.sbs/',
-  'https://158.160.74.235/',
-  'http://158.160.74.235/',
-  'https://www.158.160.74.235/',
-  'http://www.158.160.74.235/',
+  'http://mestoapi.nomoreparties.sbs',
+  'https://mestoapi.nomoreparties.sbs',
+  'http://api.mestoapi.nomoreparties.sbs',
+  'https://api.mestoapi.nomoreparties.sbs',
+  'http://www.mestoapi.nomoreparties.sbs',
+  'https://www.mestoapi.nomoreparties.sbs',
+  'http://www.api.mestoapi.nomoreparties.sbs',
+  'https://www.api.mestoapi.nomoreparties.sbs',
+  'https://158.160.74.235',
+  'http://158.160.74.235',
+  'https://www.158.160.74.235',
+  'http://www.158.160.74.235',
 ];
